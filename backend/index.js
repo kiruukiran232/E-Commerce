949d30475760e55252c181ee3e1d93ef7d87a0d6
kiruukiran232 <kirankumardevarapalli232@gmail.com>
2024-08-17 12:20:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,13 @@ const dotenv = require("dotenv").config();
 const Stripe = require('stripe')
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  
+  origin : ["https://e-commerce-pi-beige-89.vercel.app"],
+  methods : ["POST", "GET"],
+  credentials : true
+}
+));
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
